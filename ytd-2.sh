2cc573bc1a4ad8c1885f764b9d9e9c704826396b
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-03-03 07:14:46 -0300

    update download list

--- ytd-2.sh
+++ ytd-2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-youtube-dl  -ci --playlist-items $2 --extract-audio --audio-format mp3 --audio-quality 6 -o '%(title)s.%(ext)s' $1
+youtube-dl  -ci --extract-audio --audio-format mp3 --audio-quality 6 -o '%(title)s.%(ext)s' $1
 
 mv *.mp3 ../../Music/musicDontEdited
